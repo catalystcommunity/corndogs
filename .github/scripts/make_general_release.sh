fa2e42bf64c809815b9988c0d7918c780fd3633b
JacobPuff <jacob@todandlorna.com>
2024-12-13 15:20:51 -0700

    fix: add script for release, start updating workflows to handle monorepo

---
+++ .github/scripts/make_general_release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+RELEASE_NAME="${RELEASE_NAME:-$1}"
+GITHUB_TOKEN="${GITHUB_TOKEN:-notarealtoken}"
+GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-catalystcommunity/semver-tags}"
+DEFAULT_GITHUB_API_URL="https://api.github.com"
+GITHUB_API_URL="${GITHUB_API_URL:-${DEFAULT_GITHUB_API_URL}}"
+
+TARFILE_NAME="corndogs-${PACKAGE_NAME}.tar.gz"
+
+echo ${RELEASE_NAME}
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases" \
+  -d '{"tag_name":"'${RELEASE_NAME}'","target_commitish":"main","name":"'${RELEASE_NAME}'","body":"Released by Github Action","draft":false,"prerelease":false,"generate_release_notes":false}'
