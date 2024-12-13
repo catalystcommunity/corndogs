53b520a3184392cd5d0cab85c965c460ecfe5e87
JacobPuff <jacob@todandlorna.com>
2024-12-13 15:25:49 -0700

    fix: actually delete all the old corndogs files, and remove kubectl install script

--- server/implementations/v1alpha1server.go
+++
@@ -1,8 +0,0 @@
-package implementations
-
-import corndogsv1alpha1 "github.com/TnLCommunity/protos-corndogs/gen/proto/go/corndogs/v1alpha1"
-
-// implements the corndogs api
-type V1Alpha1Server struct {
-	corndogsv1alpha1.UnimplementedCorndogsServiceServer
-}
