dee187918b9d2634dd0eeb6ba7db734cdc08e9b9
JacobPuff <jacob@todandlorna.com>
2025-01-31 13:46:15 -0700

    fix: repo url

--- .github/scripts/make_helm_release.sh
+++ .github/scripts/make_helm_release.sh
@@ -27,7 +27,7 @@ TAGGED_RELEASE_ID=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/OWNER/REPO/releases/tags/${RELEASE_NAME} | yq ".id")
+   ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/tags/${RELEASE_NAME} | yq ".id")
 
 # Add chart tarfile to release
 curl -L \
