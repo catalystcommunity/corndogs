483897eb1f9748aba0ef171938e206f1e9e42f23
JacobPuff <jacob@todandlorna.com>
2025-04-10 18:39:49 -0600

    fix: testin install script

---
+++ .github/scripts/install_and_buf_generate.sh
@@ -0,0 +1,12 @@
+GOBIN=/usr/local/bin go install github.com/bufbuild/buf/cmd/buf@v1.50.1
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+python3 -m pip install --upgrade pip
+python3 -m pip install grpclib protobuf
+
+buf lint
+buf mod prune
+buf mod update
+buf generate
+buf breaking
