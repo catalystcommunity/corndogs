db851c08067403fecc8c90198343046fb0a4d46e
JacobPuff <jacob@todandlorna.com>
2025-04-10 19:45:52 -0600

    fix: actually unzip the protoc stuff

--- .github/scripts/install_and_buf_generate.sh
+++ .github/scripts/install_and_buf_generate.sh
@@ -1,6 +1,9 @@
 # install protoc
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 curl -LO $PB_REL/download/v30.2/protoc-30.2-linux-x86_64.zip
+unzip protoc-30.2-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+
 
 # install buf
 GOBIN=/usr/local/bin go install github.com/bufbuild/buf/cmd/buf@v1.50.1
