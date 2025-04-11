8288937a68b5a151623303202cf5228f120d1d53
JacobPuff <JacobPuff@users.noreply.github.com>
2025-04-10 21:50:01 -0600

    fix: generation stuff actually works (#33)
    * fix: trigger workflow
    
    * fix: configure git for workflow, move commit out of the install and generate script
    
    * fix: checkout ref to point to PR
    
    * fix: echo stuff
    
    * fix: replace protoc install stuff
    
    * fix: actually unzip the protoc stuff
    
    * fix: update protoc version
    
    * fix: conditionally commit if changes exist
    
    * fix: add the letter v
    
    * fix: add echo
    
    * fix: move to version 3.13.0 of protoc
    
    * fix: version
    
    * fix: list directory stuff
    
    * fix: ls command instead
    
    * fix: unzip the right file
    
    * fix: update version again
    
    * fix: remove echoes
    
    * fix: dont run on chore jobs
    
    * fix: cleanup zip file
    
    * fix: trigger workflow
    
    * fix: check commit message is not chore
    
    * fix: echo event thingy
    
    * fix: echo all of the event
    
    * fix: check against sender to skip workflow
    
    * chore(ci): generate buf stuff
    
    ---------
    
    Co-authored-by: Catalyst Community Automation <catalyst-community@todandlorna.com>

--- .github/scripts/install_and_buf_generate.sh
+++ .github/scripts/install_and_buf_generate.sh
@@ -1,15 +1,22 @@
+# install protoc
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v3.12.4/protoc-3.12.4-linux-x86_64.zip
+unzip protoc-3.12.4-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+rm protoc-3.12.4-linux-x86_64.zip
+
+# install buf
 GOBIN=/usr/local/bin go install github.com/bufbuild/buf/cmd/buf@v1.50.1
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+# install plugins
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.36.6
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
 python3 -m pip install --upgrade pip
 python3 -m pip install grpclib protobuf
 
+# run buf stuff
 cd protos
 buf lint
 # buf dep prune
 # buf dep update
 buf generate
-git add .
-git commit -m "chore(ci): generate buf stuff"
-git push
